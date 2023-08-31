7b9a34c39cc9be1316a2ca7fce21456e7e4ee407
Alief Zulvan <depanb187@gmail.com>
2023-08-31 21:37:43 +0700

    added praktikum modul 3

---
+++ Modul3/soap/src/main/java/com/example/soap/CountryEndpoint.java
@@ -0,0 +1,30 @@
+package com.example.soap;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+import com.example.soap.gen.GetCountryRequest;
+import com.example.soap.gen.GetCountryResponse;
+
+@Endpoint
+
+public class CountryEndpoint {
+
+    private static final String NAMESPACE_URI = "http://example.com/soap/gen";
+    private CountryRepository countryRepository;
+
+    @Autowired
+    public CountryEndpoint(CountryRepository countryRepository) {
+        this.countryRepository = countryRepository;
+    }
+
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "getCountryRequest")
+    @ResponsePayload
+    public GetCountryResponse getCountry(@RequestPayload GetCountryRequest request) {
+        GetCountryResponse response = new GetCountryResponse();
+        response.setCountry(countryRepository.findCountry(request.getName()));
+        return response;
+    }
+}
---
+++ Modul3/soap/src/main/java/com/example/soap/Soap.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.example.soap;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Soap {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
